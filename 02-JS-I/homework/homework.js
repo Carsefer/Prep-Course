5631f96218883a5d18d3d6c41837e289c3e01506
Carsefer <carlosm.rodriguez4@gmail.com>
2022-06-25 21:12:14 -0500

    version final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -251,6 +251,7 @@ function esVocal(letra) {
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" ||letra === "u"){
     return "Es vocal";
   }
+  
   return "Dato incorrecto"
 }
  
